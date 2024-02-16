ea9ebaaf262a6a1753800dacb1b87ab0519b5ece
Milos Rankovic <mrankovic310@gmail.com>
2024-02-16 16:39:22 +0100

    Progres

--- basics/index.php
+++ basics/index.php
@@ -21,6 +21,17 @@
     myTest();
 
     var_dump(__LINE__);
+
+    echo "*********************************";
+    echo "<br>" . $_SERVER['PHP_SELF'];
+    echo "<br>" .  $_SERVER['SERVER_NAME'];
+    echo "<br>" .  $_SERVER['HTTP_HOST'];
+    echo "<br>" .  $_SERVER['HTTP_REFERER'];
+    echo "<br>" .  $_SERVER['HTTP_USER_AGENT'];
+    echo "<br>" .  $_SERVER['SCRIPT_NAME'];
     
-    // PHP funcitons is next chapter
+    echo date("Y");
+
+    // PHP OOP is next chapter
+
 ?>
